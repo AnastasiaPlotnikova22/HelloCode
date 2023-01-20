4fd0c3f3758d820fe2d75f889016e7118ef9973c
Anastasia Plotnikova <nastaploton@gmail.com>
2023-01-20 11:53:00 +0300

    Example 005 Condition If Else

---
+++ Examples005_Condition_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите им пользователя: ");
+string username = Clonsole.ReadLine();
+
+if(username.ToLower() == "маша");
+{
+     Console.WriteLine("Ура, это МАША!");
+}
+else
+{
+     Console.Write("Привет, ");
+    Console.WriteLine(username);   
+}
